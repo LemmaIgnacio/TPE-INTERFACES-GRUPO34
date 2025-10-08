e118fe5f60ae841b1ce673906cb7c258fe314f82
MilagrosLv <lopezvmilagros@gmail.com>
2025-10-07 21:17:25 -0300

    Lucha con dropdown

--- TP2/app.js/app.js
+++ TP2/app.js/app.js
@@ -1,6 +1,21 @@
+const topbar = document.getElementById('topbar');
 const sidebar = document.getElementById('sidebar');
 
-// Toggle sidebar visibility in mobile view
-function toggleSidebar() {
-    sidebar.classList.toggle('show-sidebar');
-}
+const dropdownContent = document.getElementById('dropdownContent');
+const dropdownBtn = document.getElementById('dropdownBtn');
+
+// Toggle dropdown menu visibility
+    dropdownBtn?.addEventListener('click', () => {
+        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    });
+
+// Close the dropdown if the user clicks outside of it
+    window.addEventListener('click', (event) => {
+        if (event.target !== dropdownBtn && !event.target.closest('.dropdown')) {
+            if (dropdownContent) {
+                dropdownContent.style.display = 'none';
+            }
+        }
+    });
+
+
