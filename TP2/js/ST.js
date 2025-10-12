c8bc608a98a92ef9fa183219b8b3d6bccd9a5375
MilagrosLv <lopezvmilagros@gmail.com>
2025-10-12 19:43:32 -0300

    Funciona el Sidebar en Desktop y Mobile

--- TP2/js/ST.js
+++ TP2/js/ST.js
@@ -1,23 +1,28 @@
 const toggleBtn = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
+const main = document.querySelector('main')
 
 
 //Sidebar toggle
-function toggleSidebar(params) {
-    sidebar.classList.toggle('close')
-    toggleBtn.classList.toggle('rotate')
-}
+    // Toggle sidebar for Desktop
+    function toggleSidebar(params) {
+        sidebar.classList.toggle('close')
+        toggleBtn.classList.toggle('rotate')
+    }
 
-function toggleSidebarMobil(params) {
-    toggleBtn.classList.toggle('rotate')
-    sidebar.classList.remove('close')
-    sidebar.classList.toggle('show-sidebar-mobile')
-    
-}
-function closeSidebarMobile(params) {
-    sidebar.classList.remove('show-sidebar-mobile')
-    
-}
+    // Toggle sidebar for Mobile
+    function toggleSidebarMobil(params) {
+        toggleBtn.classList.toggle('rotate')
+        sidebar.classList.remove('close')
+        sidebar.classList.toggle('show-sidebar-mobile')
+        main.classList.toggle('less-opacity')
+        
+    }
+    function closeSidebarMobile(params) {
+        sidebar.classList.remove('show-sidebar-mobile')
+        main.classList.remove('less-opacity')
+        toggleBtn.classList.remove('rotate')
+    }
 
 
 //Dropdown toggle
@@ -35,13 +40,18 @@ function closeSidebarMobile(params) {
         document.querySelectorAll('.dropdown-btn.rotate').forEach(btn => btn.classList.remove('rotate'));
     }
 
-    // Close dropdowns when clicking outside any .dropdown
+
+//Event listener to close submenus when clicking outside
     document.addEventListener('click', (e) => {
         // if the click is not inside a .dropdown, close all
         if (!e.target.closest('.dropdown')) {
             closeAllSubMenus();
         }
+
+        // Close sidebar when clicking outside on mobile
         if (!e.target.closest('.show-sidebar-mobile') && !e.target.closest('#toggle-btn')) {
+            closeSidebarMobile()
+        } else if (sidebar.classList.contains('close')) {
             closeSidebarMobile()
         }
     });
